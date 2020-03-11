5b6f69ef1a0763aa4fefb62542917e4ad70687d8
mcagan <meliscagan@gmail.com>
2020-03-11 18:50:56 +0000

    refactored constants

---
+++ constants.js
@@ -0,0 +1,8 @@
+const IP = '192.168.0.103';
+const PORT = 50541;
+const MOVE_UP = "Move: up"
+const MOVE_DOWN = "Move: down"
+const MOVE_LEFT = "Move: left"
+const MOVE_RIGHT = "Move: right"
+
+module.exports = {IP, PORT, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT, MOVE_UP};
--- input.js
+++ input.js
@@ -3,6 +3,7 @@
  * Specifically, so that we can handle user input via stdin
  */
 let connection;
+const {MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT, MOVE_UP} = require('./constants');
 
 const setupInput = function(conn) {
   connection = conn;
@@ -14,19 +15,19 @@ const setupInput = function(conn) {
     if (key === '\u0003') {
       process.exit();
     } else if (key === 'w') {
-      conn.write('Move: up')
+      conn.write(MOVE_UP);
     } else if (key === 'a') {
-      conn.write('Move: left')
+      conn.write(MOVE_LEFT);
     } else if (key === 's') {
-      conn.write('Move: down')
+      conn.write(MOVE_DOWN);
     } else if (key === 'd') {
-      conn.write('Move: right')
+      conn.write(MOVE_RIGHT);
     } else if (key === 'q') {
-      conn.write('Say: Got it!')
+      conn.write('Say: Got it!');
     } else if (key === 'h') {
-      conn.write('Say: Haha')
+      conn.write('Say: Haha');
     } else if (key === 'i') {
-      conn.write('Say: I win')
+      conn.write('Say: I win');
     }
   });
   handleUserInput();
