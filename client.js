18b0065b45e3e700630d9d59eb6561491a89e07f
mcagan <meliscagan@gmail.com>
2020-03-11 17:08:12 +0000

    naming the snake

--- client.js
+++ client.js
@@ -13,6 +13,13 @@ const connect = function() {
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server.');
+  })
+
+  conn.on('connect', () => {
+    conn.write('Name: MC');
+  });
   return conn;
 }
 
