db837548033ce6d7639c158d73330d8d2452a73c
mcagan <meliscagan@gmail.com>
2020-03-11 18:23:33 +0000

    add move commands

--- input.js
+++ input.js
@@ -2,19 +2,29 @@
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
+let connection;
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  const handleUserInput = stdin.on('data', (key) => {
+  const handleUserInput = () => stdin.on('data', (key) => {
     if (key === '\u0003') {
       process.exit();
+    } else if (key === 'w') {
+      conn.write('Move: up')
+    } else if (key === 'a') {
+      conn.write('Move: left')
+    } else if (key === 's') {
+      conn.write('Move: down')
+    } else if (key === 'd') {
+      conn.write('Move: right')
     }
   });
   handleUserInput();
   return stdin;
-}
+};
 
 module.exports = {setupInput};
--- play.js
+++ play.js
@@ -2,6 +2,8 @@ const {connect} = require('./client');
 const {setupInput} = require('./input');
 
 console.log('Connecting ...');
-connect();
-setupInput();
+
+let connObj = connect();
+
+setupInput(connObj);
 
